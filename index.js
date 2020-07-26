627c462086e6f3934fd373ee99ce19af85859e7f
Evan Pavone <evanrpavone@gmail.com>
2020-07-26 16:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,14 +88,13 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
+// var act = {37: moveDodgerLeft, 38: moveDodgerRight};
+  if (e.which == 37 || e.which == 39) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.which == 37 ? moveDodgerLeft() : false;
+    e.which == 39 ? moveDodgerRight() : false;
+  }
 }
 
 function moveDodgerLeft() {
