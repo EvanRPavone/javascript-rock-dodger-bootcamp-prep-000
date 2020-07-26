d87db2853654395acc3f3e6198a48ece8675e39e
Evan Pavone <evanrpavone@gmail.com>
2020-07-26 16:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,44 +50,25 @@ function createRock(x) {
 
   var top = 0;
 
-  rock.style.top = top
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame().
-     */
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM.
-     */
+    rock.style.top= `${top += 2}px`;
+    if (checkCollision(rock)) {
+      endGame();
+    }
+    if (top<=GAME_HEIGHT) {
+      rock.remove();
+    } else {
+      window.requestAnimationFrame(moveRock);
+    }
   }
+}
 
-  // We should kick off the animation of the rock around here.
+window.requestAnimationFrame(moveRock);
 
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision.
+
   ROCKS.push(rock)
 
-  // Finally, return the rock element you've created.
+
   return rock
 }
 
@@ -98,6 +79,12 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  document.removeEventListener('keydown', moveDodger);
+  window.clearInterval(gameInterval);
+  ROCKS.forEach(function(rock){
+    rock.remove();
+  });
+  alert("YOU LOSE!");
 }
 
 function moveDodger(e) {
